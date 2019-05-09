1319881b7b82a1294a2e75447e69bdf565f8da9d
justin <justiny.ahinon@gmail.com>
2019-05-09 09:48:57 +0100

    create first component of the plugin admin page

--- admin/partials/wpmassistant-admin-display.php
+++ admin/partials/wpmassistant-admin-display.php
@@ -13,4 +13,32 @@
  */
 ?>
 
-<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+<h2><?php echo get_admin_page_title(); ?></h2>
+
+<?php
+/**
+ * Query to retrieve all the images uploaded in WordPress gallery
+ * 
+ * @since	1.0.0
+ */
+
+function image_all() {
+    $query_images_args = array(
+        'post_type'      => 'attachment',
+        'post_mime_type' => 'image',
+        'post_status'    => 'inherit',
+        'posts_per_page'  => '-1',   
+    );
+    
+    $query_images = new WP_Query( $query_images_args );
+    
+    $images = $query_images;
+    foreach ( $images->posts as $image_info ) {
+        $image_info = wp_get_attachment_url( $image_info->ID ) . "<br>" ;
+        echo the_attachment
+        echo $image_info;
+    }
+
+}
+
+image_all();
