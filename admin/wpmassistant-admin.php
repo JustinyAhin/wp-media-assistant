09907bb8aa00a9a576813711bc194ead0ea547d4
justin <justiny.ahinon@gmail.com>
2019-05-29 13:12:36 +0100

    set up the plugin to retrieve the list of images uploaded

--- admin/wpmassistant-admin.php
+++ admin/wpmassistant-admin.php
@@ -22,15 +22,52 @@
 
  // Enqueue scripts
 
-// Add the plugin admin menu
+// Add the plugin admin main menu
 add_action( 'admin_menu', 'wpmassistant_admin_menu' );
 function wpmassistant_admin_menu() {
     add_menu_page(
         __( 'WP Media Assistant Dashboard', 'wpmassistant' ),
-        'WPMA Dashboard',
+        'WPMA',
         'manage_options',
         'wpma_dashboard',
-        'wpma-dashboard-page',
+        'wpma_dashboard_page',
         'dashicons-welcome-widgets-menus'
     );
+}
+
+// The function to retrieve all the medias uploaded in the media library
+function wpma_retrieve_images() {
+    $query_images_args = array(
+        'post_type'      => 'attachment',
+        'post_mime_type' => 'image',
+        'post_status'    => 'inherit',
+        'posts_per_page'  => '-1',   
+    );
+    
+    $query_images = new WP_Query( $query_images_args );
+    
+    $wpma_medias_url_list = [];
+    foreach ( $query_images->posts as $image_info ) {
+        $wpma_medias_url_list [] = wp_get_attachment_url( $image_info->ID );
+    }
+    return $wpma_medias_url_list;
+}
+
+// The code to display the plugin admin page
+function wpma_dashboard_page() {
+    ?>
+    <div class="wpma-dsh-wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <p>This page will display a set of useful informations about the files in your media library</p>
+    </div> 
+    <div>
+        <?php 
+        $medias_url_list = wpma_retrieve_images();
+        foreach ( $medias_url_list as $single_image_url ) {
+            echo $single_image_url . "<br>";
+        }
+        ?>
+    </div>
+    
+    <?php
 }
